64da7af3b50877a0d626eca0799b2b559e1280bf
vidina <accounts@vidina.solutions>
2023-06-07 17:42:37 +0000

    added build and apt install scripts

---
+++ apt_install.sh
@@ -0,0 +1,3 @@
+sudo apt-get install build-essential git python3-dev python3-pip libopenexr-dev libxi-dev \
+                     libglfw3-dev libglew-dev libomp-dev libxinerama-dev libxcursor-dev
+
